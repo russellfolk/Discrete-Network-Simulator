08a582f8aac7a4e8eaeb0541733f73e123409ee3
Russell Folk <rfolk@russellfolk.com>
2013-11-29 20:23:25 -0700

    Added constants and refactored the functions.
    Needed to be able to use a 1.0 for lambda for Poisson Distribution. Added the
    defined queue size of 30. Added checks to remove chance for duplicate code.

--- include/dns_constants.h
+++ include/dns_constants.h
@@ -2,13 +2,14 @@
 	* Fixed constants used throughout the Discrete Network Simulator.
 	*
 	*	Created: 18 November 2013
-	*	Revised:
+	*	Revised: 29 November 2013
 	*/
 
 #ifndef __PROGRAM_CONSTANTS__
 	#define __PROGRAM_CONSTANTS__
 
 	const int NUMBER_OF_NODES = 150 ;
+	const int QUEUE_SIZE = 30 ;
 	const double EULER = 2.71828182845904523536f ;
 	const double LAMBDA = 0.5f ;
 
--- include/poisson_random_variable.h
+++ include/poisson_random_variable.h
@@ -5,14 +5,21 @@
 	*	Distribution
 	*
 	*	Created: 18 November 2013
-	*	Revised:
+	*	Revised: 29 November 2013
 	*/
 
 #ifndef __POISSON_RANDOM_VARIABLE__
 	#define __POISSON_RANDOM_VARIABLE__
 
-	#include "uniform_random_variable.h"
-	#include "dns_constants.h"
+	#ifndef __UNIFORM_RANDOM__
+		#define __UNIFORM_RANDOM__
+		#include "uniform_random_variable.h"
+	#endif
+
+	#ifndef __CONSTANTS__
+		#define __CONSTANTS__
+		#include "dns_constants.h"
+	#endif
 
 	#ifndef __CSTDLIB__
 		#define __CSTDLIB__
@@ -30,11 +37,24 @@
 		*	Returns a variable x that is generated using the Poisson Distribution with
 		*	a set variable lambda that defines the exponential random variable.
 		*
-		* @return variable x
+		* @return double variable x
 		*/
 	double poisson_random_variable ()
 	{
-		return ( -1.0f / LAMBDA ) * log ( standard_uniform_random_variable() ) ;
+		return ( -1.0f / LAMBDA ) * poisson_random_variable() ;
 	}
+
+	/**
+		*	Poisson Distribution Random Variable
+		*	Returns a variable x that is generated using the Poisson Distribution with
+		*	a set variable lambda of 1.0 that defines the exponential random variable.
+		*
+		* @return double variable x
+		*/
+	double poisson_random_variable_1 ()
+	{
+		return log ( standard_uniform_random_variable() ) ;
+	}
+
 
 #endif
